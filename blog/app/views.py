3e20ce59f314d126c2404df3fb3f2f0d49bba2ca
github_saish17 <yeolesaish777@gmail.com>
2022-05-22 23:05:03 +0530

    first commit on Blog Application

---
+++ blog/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+def about_view(request):
+    return render(request,'about.html')
+
+def contact_view(request):
+    return render(request,'contact.html')    
